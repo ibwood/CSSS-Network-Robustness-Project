c6a7a10645581d38cf5bc66ee72e1a370a697799
Ian Wood <ibwood@indiana.edu>
2012-06-17 16:59:37 -0600

    7

--- Rule.py
+++ Rule.py
@@ -1,9 +1,9 @@
 class Rule(object):
 	def __init__(self):
 		print "Implement!"
-	def run(self, n):
+	def run(self, n, **args):
 		print "Must Implement run!"
-	def run_nodes(self, nodes):
+	def run_nodes(self, nodes, **args):
 		print "Must Implement run_nodes!"
 	def set_new_network(self, net):
 		print "Must Implement set_new_network!"
