b0c88a6bc951badbece9ab6b4b15397d0db19d73
Ian Wood <ibwood@indiana.edu>
2012-06-17 21:42:02 -0600

    Merged

--- GrowthRule.py
+++
@@ -1,9 +0,0 @@
-class GrowthRule(object):
-	def __init__(self):
-		print "Implement!"
-	def run(self, n):
-		print "Must Implement run!"
-	def run_nodes(self, nodes):
-		print "Must Implement run_nodes!"
-	def set_new_network(self, net):
-		print "Must Implement set_new_network!"
